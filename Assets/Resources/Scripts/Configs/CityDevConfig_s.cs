28cfc375a804cdfd1c023947e2d652052fb9c86b
narlon <narlonzjh@163.com>
2026-03-07 10:59:16 +0800

    feat(配置): 更新城市发展配置并添加新功能
    - 新增交易功能及相关配置项
    - 调整城市发展配置ID结构
    - 更新相关视频和图片资源
    - 修改注释字段"图标"为"行动"

--- Assets/Resources/Scripts/Configs/CityDevConfig_s.cs
+++ Assets/Resources/Scripts/Configs/CityDevConfig_s.cs
@@ -71,7 +71,7 @@ namespace CommonConfig
         /// </summary>
         public string[] Attrs;
         /// <summary>
-        ///图标
+        ///行动
         /// </summary>
         public string ActionName;
         /// <summary>
@@ -121,14 +121,15 @@ namespace CommonConfig
         {
             config.Clear();
             config[21001] = new CityDevConfig(21001, "farm", "发展农业", "发展农业,提升粮食产量", "CityDevNormal", false, "farm", 300, 3, "ArchFood", new int[]{4,10}, "ArchPeople", new int[]{300,800}, "", null, new string[]{"Fair","Str"}, "dev", "farm.mp4");
-            config[21002] = new CityDevConfig(21002, "market", "发展商业", "发展商业,提升金钱收入", "CityDevNormal", false, "market", 300, 3, "ArchGold", new int[]{4,10}, "ArchPeople", new int[]{300,800}, "", null, new string[]{"Fair","Inte"}, "dev", "shop3.mp4");
-            config[21003] = new CityDevConfig(21003, "gate", "加固城墙", "提升城防", "CityDevNormal", false, "wall", 200, 3, "Wall", new int[]{6,15}, "Secure", new int[]{1,3}, "", null, new string[]{"Str"}, "def", "fix3.mp4");
-            config[21004] = new CityDevConfig(21004, "market", "街道巡逻", "提升治安", "CityDevNormal", false, "secure", 150, 3, "Secure", new int[]{2,6}, "ArchPeople", new int[]{200,500}, "", null, new string[]{"Str"}, "dev", "secure2.mp4");
-            config[21005] = new CityDevConfig(21005, "market", "走访", "搜集人才和宝物", "CityDevNormal", false, "find", 0, 10, "Gold", new int[]{20,100}, "", null, "", null, new string[]{"Charm","Inte"}, "find", "search2.mp4");
-            config[21006] = new CityDevConfig(21006, "army", "训练", "提升军队士气", "CityDevNormal", false, "train", 0, 3, "Power", new int[]{3,9}, "", null, "", null, new string[]{"LeadShip","Str"}, "sod", "train3.mp4");
-            config[21007] = new CityDevConfig(21007, "army", "征兵", "提升士兵数量", "CityDevNormal", false, "zhengbing", 300, 3, "Soldier", new int[]{200,600}, "Secure", new int[]{-2,-5}, "ArchPeople", new int[]{-500,-1500}, new string[]{"LeadShip","Charm"}, "sod", "zhengbing2.mp4");
-            config[21008] = new CityDevConfig(21008, "army", "出战", "出兵攻打敌人", "CityDevBattle", true, "battle", 0, 10, "", null, "", null, "", null, new string[]{"LeadShip","Str"}, "", "atk2.mp4");
-            config[21009] = new CityDevConfig(21009, "gate", "移动", "移动到其他城市", "CityDevBattle", false, "move", 0, 10, "", null, "", null, "", null, new string[]{"LeadShip","Charm"}, "", "move2.mp4");
+            config[21002] = new CityDevConfig(21002, "farm", "发展商业", "发展商业,提升金钱收入", "CityDevNormal", false, "market", 300, 3, "ArchGold", new int[]{4,10}, "ArchPeople", new int[]{300,800}, "", null, new string[]{"Fair","Inte"}, "dev", "shop3.mp4");
+            config[21101] = new CityDevConfig(21101, "gate", "加固城墙", "提升城防", "CityDevNormal", false, "wall", 200, 3, "Wall", new int[]{6,15}, "Secure", new int[]{1,3}, "", null, new string[]{"Str"}, "def", "fix4.mp4");
+            config[21102] = new CityDevConfig(21102, "gate", "移动", "移动到其他城市", "CityDevBattle", false, "move", 0, 10, "", null, "", null, "", null, new string[]{"LeadShip","Charm"}, "", "move2.mp4");
+            config[21103] = new CityDevConfig(21103, "army", "出战", "出兵攻打敌人", "CityDevBattle", true, "battle", 0, 10, "", null, "", null, "", null, new string[]{"LeadShip","Str"}, "", "atk2.mp4");
+            config[21201] = new CityDevConfig(21201, "market", "街道巡逻", "提升治安", "CityDevNormal", false, "secure", 150, 3, "Secure", new int[]{2,6}, "ArchPeople", new int[]{200,500}, "", null, new string[]{"Str"}, "dev", "secure2.mp4");
+            config[21202] = new CityDevConfig(21202, "market", "走访", "搜集人才和宝物", "CityDevNormal", false, "find", 0, 10, "Gold", new int[]{20,100}, "", null, "", null, new string[]{"Charm","Inte"}, "find", "search2.mp4");
+            config[21203] = new CityDevConfig(21203, "market", "交易", "交易钱粮", "CityDevChange", false, "change", 0, 1, "", null, "", null, "", null, new string[]{"Inte"}, "", "change.mp4");
+            config[21301] = new CityDevConfig(21301, "army", "训练", "提升军队士气", "CityDevNormal", false, "train", 0, 3, "Power", new int[]{3,9}, "", null, "", null, new string[]{"LeadShip","Str"}, "sod", "train3.mp4");
+            config[21302] = new CityDevConfig(21302, "army", "征兵", "提升士兵数量", "CityDevNormal", false, "zhengbing", 300, 3, "Soldier", new int[]{200,600}, "Secure", new int[]{-2,-5}, "ArchPeople", new int[]{-500,-1500}, new string[]{"LeadShip","Charm"}, "sod", "zhengbing2.mp4");
 
 
 
