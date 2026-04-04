0fcd826ed275dbf6746695f89d31ed00530d64b8
narlon <narlonzjh@163.com>
2026-04-04 22:17:35 +0800

    fix(config): 更新城市发展配置和建筑纹理
    调整CityDevConfig中的移动和褒奖配置参数
    更新多个建筑纹理图片并添加praise.jpg资源

--- Assets/Resources/Scripts/Configs/CityDevConfig_s.cs
+++ Assets/Resources/Scripts/Configs/CityDevConfig_s.cs
@@ -133,7 +133,7 @@ namespace CommonConfig
             config[21001] = new CityDevConfig(21001, "farm", "发展农业", "发展农业,提升粮食产量", "CityDevNormal", "farm", 300, 3, "ArchFood", new int[]{4,10}, "ArchPeople", new int[]{300,800}, "", null, new string[]{"Fair","Str"}, "dev", 11, 0, 0, "farm.mp4");
             config[21002] = new CityDevConfig(21002, "farm", "发展商业", "发展商业,提升金钱收入", "CityDevNormal", "market", 300, 3, "ArchGold", new int[]{4,10}, "ArchPeople", new int[]{300,800}, "", null, new string[]{"Fair","Inte"}, "dev", 10, 0, 0, "shop3.mp4");
             config[21101] = new CityDevConfig(21101, "gate", "加固城墙", "提升城防", "CityDevNormal", "wall", 200, 3, "Wall", new int[]{6,15}, "Secure", new int[]{1,3}, "", null, new string[]{"Str"}, "def", 30, 0, 10, "fix4.mp4");
-            config[21102] = new CityDevConfig(21102, "gate", "移动", "移动到其他城市", "CityDevMove", "move", 0, 10, "", null, "", null, "", null, new string[]{"LeadShip","Charm"}, "", 2, 5, 3, "move2.mp4");
+            config[21102] = new CityDevConfig(21102, "gate", "移动", "移动到其他城市", "CityDevMove", "move", 0, 10, "", null, "", null, "", null, new string[]{"LeadShip","Charm"}, "", 0, 4, 0, "move2.mp4");
             config[21103] = new CityDevConfig(21103, "army", "出战", "出兵攻打敌人", "CityDevBattle", "battle", 0, 10, "", null, "", null, "", null, new string[]{"LeadShip","Str"}, "", 0, 1, 0, "atk2.mp4");
             config[21201] = new CityDevConfig(21201, "market", "街道巡逻", "提升治安", "CityDevNormal", "secure", 150, 3, "Secure", new int[]{2,6}, "ArchPeople", new int[]{200,500}, "", null, new string[]{"Str"}, "dev", 20, 10, 20, "secure2.mp4");
             config[21202] = new CityDevConfig(21202, "market", "走访", "搜集人才和宝物", "CityDevNormal", "find", 0, 10, "Gold", new int[]{20,100}, "", null, "", null, new string[]{"Charm","Inte"}, "find", 21, 0, 11, "search2.mp4");
@@ -141,7 +141,7 @@ namespace CommonConfig
             config[21301] = new CityDevConfig(21301, "army", "训练", "提升军队士气", "CityDevNormal", "train", 0, 3, "Power", new int[]{3,9}, "", null, "", null, new string[]{"LeadShip","Str"}, "sod", 0, 9, 3, "train3.mp4");
             config[21302] = new CityDevConfig(21302, "army", "征兵", "提升士兵数量", "CityDevNormal", "zhengbing", 300, 3, "Soldier", new int[]{200,600}, "Secure", new int[]{-2,-5}, "ArchPeople", new int[]{-500,-1500}, new string[]{"LeadShip","Charm"}, "sod", 0, 8, 2, "zhengbing2.mp4");
             config[21401] = new CityDevConfig(21401, "house", "登用", "提拔在野武将", "CityDevUseHero", "wild", 0, 1, "", null, "", null, "", null, new string[]{"Charm","Inte"}, "", 2, 0, 0, "wild.mp4");
-            config[21402] = new CityDevConfig(21402, "house", "褒奖", "提升武将忠心度", "CityDevPraiseHero", "wild", 100, 10, "", null, "", null, "", null, new string[]{"Charm","Inte"}, "", 3, 12, 0, "wild.mp4");
+            config[21402] = new CityDevConfig(21402, "house", "褒奖", "提升武将忠心度", "CityDevPraiseHero", "praise", 100, 10, "", null, "", null, "", null, new string[]{"Charm","Inte"}, "", 3, 12, 0, "wild.mp4");
 
 
 
