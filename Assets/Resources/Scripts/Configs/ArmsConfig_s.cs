16b621f2ed049de7238f27d57b44cf2fd495fbee
narlon <narlonzjh@163.com>
2026-07-08 09:00:15 +0800

    feat(arms): 新增藤甲兵种并完善相关配置
    1. 新增604号藤甲兵种配置，调整原有刀兵攻击动画时长
    2. 添加对应兵种的材质、贴图、预制体资源
    3. 更新GM测试部队配置，注释部分默认测试兵种

--- Assets/Resources/Scripts/Configs/ArmsConfig_s.cs
+++ Assets/Resources/Scripts/Configs/ArmsConfig_s.cs
@@ -188,9 +188,10 @@ namespace CommonConfig
             config[1] = new ArmsConfig(1, "dyb", "动员兵", ArmsType.SodWalk, 0, 0, 0, 10, 17, 0, 0f, "SwordHitYellowCritical", "SodStick", 1f, 4, 0, 0, 0, 0, true, 1);
             config[101] = new ArmsConfig(101, "ma", "骑兵", ArmsType.SodHorse, 1, 35, 5, 10, 17, 0, 0f, "SwordHitYellowCritical", "SodHorseSpear", 1f, 6, 2, 0, 0, 0, true, 1);
             config[201] = new ArmsConfig(201, "gong", "弓兵", ArmsType.SodBow, 1, 10, 0, 10, 40, 40, 5f, "BulletExplosionBlue", "SodBow", 1f, 4, 0, 0, 1, 0, true, 1);
-            config[601] = new ArmsConfig(601, "dao", "刀", ArmsType.SodWalk, 1, 10, 8, 10, 17, 0, 0f, "SwordHitYellowCritical", "SodDao", 0f, 4, 0, 1, 0, 0, true, 2);
+            config[601] = new ArmsConfig(601, "dao", "刀", ArmsType.SodWalk, 1, 10, 8, 10, 17, 0, 0f, "SwordHitYellowCritical", "SodDao", 1f, 4, 0, 1, 0, 0, true, 2);
             config[602] = new ArmsConfig(602, "daoqiang", "枪", ArmsType.SodWalk, 1, 20, 10, 10, 17, 0, 0f, "SwordHitYellowCritical", "SodSpear", 1f, 4, 0, 1, 1, 0, true, 1);
             config[603] = new ArmsConfig(603, "daoji", "戟", ArmsType.SodWalk, 1, 10, 20, 10, 17, 0, 0f, "SwordHitYellowCritical", "SodHalberd", 1f, 4, 0, 1, 1, 0, true, 1);
+            config[604] = new ArmsConfig(604, "teng", "藤甲", ArmsType.SodWalk, 1, 0, 40, 10, 17, 0, 0f, "SwordHitYellowCritical", "SodTeng", 1f, 4, 0, 0, 0, 99, true, 1);
             config[901] = new ArmsConfig(901, "jianta", "箭塔", ArmsType.SodBow, 1, 0, 0, 10, 45, 60, 10f, "BulletExplosionBlue", "SodBow", 1f, 4, 0, 0, 1, 0, false, 1);
 
             RebuildIndex();
